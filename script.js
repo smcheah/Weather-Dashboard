49c641df5b4b78d859f19256ea21e6df36b2aef4
Simone Cheah <simone_cheah@hotmail.com>
2020-09-14 02:23:42 +1000

    finished functionality

--- script.js
+++ script.js
@@ -1,65 +1,47 @@
-// save search history
+var date = new Date().toDateString().slice(4);
+var searchHistory = [];
+var searchInput = "";
 
+// load local storage
+loadHistory();
 
-// set latest search as default values
-var searchInput = "Melbourne, AU"
-
-// for all classes with fas => change to correct icon
-{/* <i class="fas fa-cloud"></i>
-<i class="fas fa-cloud-sun"></i>
-<i class="fas fa-cloud-sun-rain"></i>
-<i class="fas fa-cloud-showers-heavy"></i>
-<i class="fas fa-snowflake"></i>
-<i class="fas fa-wind"></i>
-<i class="fas fa-bolt"></i>
-<i class="fas fa-sun"></i> */}
-function changeIcons() {
-    switch (iconCode) {
-        case 2:
-            // storm
-            break;
-        case 3: 
-            // light rain
-            break;
-        case 5:
-            //rain
-            break;
-        case 6:
-            // snow
-            break;
-        case 7:
-            // fog / haze
-            break;
-        case 800:
-            // clear
-            break;
-        case 801:
-        case 802:
-            // few clouds
-            break;
-        case 803:
-        case 804:
-            //cloudy
-            break;
-        default:
-            // precipitation - 900
-            break;
-    }
+// default values on first open
+if (searchHistory.length === 0) {
+    searchInput = "Melbourne, AU";
+    displayData(searchInput);
+} else {
+    searchInput = searchHistory[0].cityName + ", " + searchHistory[0].countryCode
+    displayData(searchInput);
 }
 
+// when history is clicked, trigger display data with chosen search input
+$("#searchHistory").on("click", function(event) {
+    if( event.target.matches("button")) {
+        searchInput = event.target.textContent;
+        displayData(searchInput);
+    }
+})
+
 // on search click
-$("#searchBtn").on("click", (event) => {
+$("#searchBtn").on("click", function(event) {
     event.preventDefault();
-    if ($("#searchInput").val() !== "") {
+    
+    if ($("#searchInput").val().trim() !== "") {
         // if searchInput is not empty
         searchInput = $("#searchInput").val().trim();
+    } else {
+        return; // searchInput = searchHistory[0].cityName + ", " + searchHistory[0].countryCode
     }
-    
-    var queryParams = {
-        key: "8febbce1a0e54114bf39a5d36e0029c1",
-        city: searchInput, // cityName, COUNTRY
-    }
+    displayData(searchInput);
+})
 
+function displayData(input) {
+    var queryParams = {
+        key: "8febbce1a0e54114bf39a5d36e0029c1"
+    }
+    if (input) {
+        queryParams.city = input;
+    }
     var currentWeatherURL = "https://api.weatherbit.io/v2.0/current?" + $.param(queryParams);
     var forecastWeatherURL = "https://api.weatherbit.io/v2.0/forecast/daily?" + $.param(queryParams);
 
@@ -68,12 +50,17 @@ $("#searchBtn").on("click", (event) => {
         url: currentWeatherURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        // console.log(response)
         var data = response.data[0];
-        $("#cityName").text(data.city_name + ", " + data.country_code + " (" + data.ob_time);
-        $("#weather-desc").text(data.weather.code + " - " + data.weather.description)
+        var icon = (data.weather.code).toString();
+        changeIcon($(".main-image"), icon)
+
+        addToHistory((data.city_name), (data.country_code))
+
+        $("#cityName").text(data.city_name + ", " + data.country_code);
+        $("#weather-desc").text(date + " | " + data.weather.description)
         $("#temp").text(data.temp)
-        $("#uv-index").text(data.uv)
+        $("#uv-index").text((data.uv).toFixed(2))
         $("#wind-speed").text(data.wind_spd + "m/s")
         $("#humidity").text(data.rh + "%")
     })
@@ -83,12 +70,91 @@ $("#searchBtn").on("click", (event) => {
         url: forecastWeatherURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        // console.log(response)
         var data = response.data;
+        var nextDay;
+        
         for (var i = 1; i < 6; i++) {
-            $("#forecast" + i).text(data[i].datetime)
-            $("#temp" + i).text(data[i].temp)
+            nextDay = new Date(new Date().setDate(new Date().getDate() + i))
+            $("#forecast" + i).text(nextDay.toDateString().slice(4))
+
+            var icon = (data[i].weather.code).toString();
+            changeIcon($(".icon" + i), icon)
+
+            $("#temp" + i).text(data[i].max_temp + " / " + data[i].min_temp)
             $("#humidity" + i).text(data[i].rh + "%")
         }
     })
-})
+}
+
+// save search searchHistory (city/country name)
+function loadHistory() {
+    var savedsearchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    if (savedsearchHistory !== null) {
+        searchHistory = savedsearchHistory;
+    }
+
+    searchHistory.forEach(element => {
+        var newButton = $("<button>");
+        newButton.text(element.cityName + ", " + element.countryCode);
+        $("#searchHistory").append(newButton)
+    });
+}
+
+function addToHistory(cityName, countryCode) {
+    var found = searchHistory.find( element => element.cityName === cityName && element.countryCode === countryCode )
+
+    var location = searchHistory.findIndex( element => element.cityName === cityName && element.countryCode === countryCode )
+
+    // if it doesn't exist
+    if ( found === undefined) {
+        searchHistory.unshift({cityName, countryCode});
+
+        if ( searchHistory.length > 6 ) {
+            searchHistory.pop();
+        }
+
+        // adds new button
+        var newButton = $("<button>");
+        newButton.text(cityName + ", " + countryCode);
+        $("#searchHistory").prepend(newButton)
+        
+        // adds item to local storage
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory))
+
+    } else {
+        if (location !== 0) {
+            searchHistory.splice(location, 1);
+            searchHistory.unshift({cityName, countryCode})
+
+            $("#searchHistory").empty()
+            searchHistory.forEach(element => {
+                var newButton = $("<button>");
+                newButton.text(element.cityName + ", " + element.countryCode);
+                $("#searchHistory").append(newButton)
+            });
+        }
+    }
+}
+
+function changeIcon(element, icon) {
+    if (icon.startsWith("2")) {
+        element.addClass("fa-bolt")
+    } else if (icon.startsWith("3")) {
+        element.addClass("fa-cloud-sun-rain")
+    } else if (icon.startsWith("5")) {
+        element.addClass("fa-cloud-showers-heavy")
+    } else if (icon.startsWith("6")) {
+        element.addClass("fa-snowflake")
+    } else if (icon.startsWith("7")) {
+        element.addClass("fa-smog")
+    } else if (icon.startsWith("800")) {
+        element.addClass("fa-sun")
+    } else if (icon.startsWith("801") || icon.startsWith("802")) {
+        element.addClass("fa-cloud-sun")
+    } else if (icon.startsWith("803") || icon.startsWith("804")) {
+        element.addClass("fa-cloud")
+    } else {
+        element.addClass("fa-umbrella")
+    }
+}
